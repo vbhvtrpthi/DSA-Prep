dc605fe54335445246bc690ccc727a99eddc1305
Vaibhav Tripathi <vbhvtrpthi@gmail.com>
2023-10-18 05:22:50 +0530

    Basics of prog

---
+++ Level 1/Graphs/subscribe.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class subscribe {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);    //System.in is a standard input stream  
+        int n = sc.nextInt();
+        System.out.println("the number is" + n);
+        sc.close();
+    }
+}
---
+++ Level 1/Test.java
@@ -0,0 +1,8 @@
+
+public class Test {
+
+    public static void main(String[] args) throws Exception {
+         System.out.println(-7 % 1);
+    }
+  
+}
